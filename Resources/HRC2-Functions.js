a34e915ee1a4c1f67082434acb9026e1ff002780
zelon88 <zelon88@gmail.com>
2018-08-19 17:43:59 -0400

    v1.0 - Initial commit.
    -v1.0.
    -Add HRScan2 to Github.

---
+++ Resources/HRC2-Functions.js
@@ -0,0 +1,21 @@
+// / The following code toggles the visisbility of the selected object between block and none.
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'none')
+     e.style.display = 'block';
+  else
+     e.style.display = 'none'; }
+
+// / The following code toggles the visisbility of the selected object between inline-block and none.
+function toggle_visibility_inline(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'none')
+     e.style.display = 'inline-block';
+  else
+     e.style.display = 'none'; }
+
+// / The following code gives a nice onclick function for making a back button.
+function goBack() {
+  window.history.back(); }
+
+
